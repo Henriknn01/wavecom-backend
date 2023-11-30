fec44119571142622e3326cffacc9715d5555824
Henrik <norheimhenrik@gmail.com>
2023-11-30 14:13:10 +0100

    Fixed permissions for servers view and list

---
+++ pb_migrations/1701349951_updated_servers.js
@@ -0,0 +1,16 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("qyuxogp5bw5r0p2")
+
+  collection.viewRule = "@request.auth.id != \"\" && (@request.auth.id ?~ members.id || @request.auth.id ?~ admins.id || @request.auth.id = owner.id)"
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("qyuxogp5bw5r0p2")
+
+  collection.viewRule = "@request.auth.id != \"\""
+
+  return dao.saveCollection(collection)
+})
